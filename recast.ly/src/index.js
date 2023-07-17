9f03f7c1cc015f8232c7daeab84861ae2d04ec9e
Rachel Miller <rkmiller131@gmail.com>
2023-07-17 15:55:40 -0700

    Added recast.ly, implemented hard coded data

---
+++ recast.ly/src/index.js
@@ -0,0 +1,9 @@
+// TODO: Render the `App` component to the DOM
+// import { createRoot } from 'react-dom/client';
+// import App from './components/App.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('app'));
+// root.render(<App />);
+
+import App from './components/App.js';
+ReactDOM.render(<App/>, document.getElementById('app'));
